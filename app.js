88555fb72c4f734668818f1847c7a0330ddce184
Sean <tan.yong@husky.neu.edu>
2019-09-30 11:47:32 +0000

    add process.env.port instead

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ app.use("/", indexRoutes);
 app.use("/restaurants",restaurantRoutes);
 app.use("/restaurants/:id/comments",commentRoutes);
 
-app.listen(3000, function(){
-	console.log("Server listening on port 3000.");
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log("Server started.");
 });
